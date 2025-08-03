3efffc00403bc2b7d916bad83a9dd83368978d33
Long <longgg11245@gmail.com>
2025-08-03 16:52:08 +0700

    del noise comment in modules

--- models/modules.py
+++ models/modules.py
@@ -72,17 +72,15 @@ class PositionalEncoding(nn.Module):
         self.d_model = d_model
         self.linear = nn.Linear(2 * d_model, d_model)
     def get_pe(self, seq_len: int) -> torch.Tensor:
-        # Create a matrix of shape (seq_len, d_model)
         pe = torch.zeros(seq_len, self.d_model)
-        # Create a vector of shape (seq_len)
+        
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
-        # Create a vector of shape (d_model)
+        
         div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-math.log(10000.0) / self.d_model)) # (d_model / 2)
-        # Apply sine to even indices
+        
         pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
-        # Apply cosine to odd indices
         pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
-        # Add a batch dimension to the positional encoding
+        
         pe = pe.unsqueeze(0) # (1, seq_len, d_model)
         return pe
 

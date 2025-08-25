a756f957d537185e553c7403ddd508901c903180
Long <longgg11245@gmail.com>
2025-08-25 23:45:27 +0700

    update

--- models/decoder.py
+++ models/decoder.py
@@ -55,7 +55,7 @@ class AcousticDecoder(nn.Module):
             h[0], c[0] = self.rnn[0](rnn_input, (h[0], c[0]))
             for i in range(1, self.num_layers):
                 new_h, new_c = self.rnn[i](h[i-1], (h[i], c[i]))
-                h[i] = self.var_dropout(new_h) + h[i-1]
+                h[i] = self.var_dropout(new_h)
                 c[i] = new_c
             query = h[-1].unsqueeze(1).unsqueeze(1)  # [B, 1, 1, hidden]
             key = value = encoder_outputs.unsqueeze(1)  # [batch, 1, time, hidden]
